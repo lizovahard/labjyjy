ef18315e7d3ec29939902568657c384f99aae9e7
LizovaAA <lizovaaa@mer.ci.nsu.ru>
2022-10-19 14:35:58 +0700

    готова 1\3 лабы

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,6 +12,9 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
     val EDIT_TEXT_KEY = "EDIT_TEXT_KEY"
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -18,11 +22,37 @@ class MainActivity : AppCompatActivity() {
         outState.putString(EDIT_TEXT_KEY, editText.text.toString())
         super.onSaveInstanceState(outState)
     }
+
+
+    companion object {
+        const val EXTRA_KEY = "EXTRA"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button5)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
 
+            intent.putExtra(EXTRA_KEY, "1")
+            startActivity(intent)
+        }
+        val button2 = findViewById<Button>(R.id.button6)
+        button2.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra(EXTRA_KEY, "2")
+            startActivity(intent)
+        }
+        val button3 = findViewById<Button>(R.id.button7)
+        button3.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra(EXTRA_KEY, "3")
+            startActivity(intent)
+        }
         if (savedInstanceState != null) {
             val editText = findViewById<TextView>(R.id.tv)
             editText.text = (savedInstanceState.getString(EDIT_TEXT_KEY))
